78f02b96c1b2f8eb5a43ad42b4b7cfddb7536233
cloudcoke <lms4296@gmail.com>
2023-03-24 16:17:38 +0900

    faf

--- back/server.js
+++ back/server.js
@@ -1,5 +1,10 @@
 const express = require("express")
 const app = express()
+const test = require("./tset")
+
+app.get("/", (req, res, next) => {
+  res.send(test)
+})
 
 app.listen(3031, () => {
   console.log("Server Start 3031")
